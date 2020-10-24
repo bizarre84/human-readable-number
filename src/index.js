3de7716a8b363c2ebbe7784b8d5ecf81abc9565f
bizarre84 <bizarre.san@gmail.com>
2020-10-24 13:29:02 +0300

    feat: impliment the func from 1 to 100

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function toReadable (number) {
+  let dic = {
+      digits: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen' ,'nineteen'
+    ],
+    decades: ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  }
+  let arrDigits = number.toString().split('').map(Number)
+
+  let toReadable20 = (number) => dic.digits[number]
+  let toReadable99 = (number) => {
+    return (number%10 != 0) ? `${dic.decades[arrDigits[0]-2]} ${dic.digits[arrDigits[1]]}` : `${dic.decades[arrDigits[0]-2]}`
+  }
   
+  
+  
+
+  if (number < 20) return toReadable20(number)
+  if (number >=20 && number < 100) return toReadable99(number)
+  if (number >=100 && number < 1000) {
+    if (number%100 == 0) {return `${dic.digits[arrDigits[0]]} hundred`}
+    if ((number - (arrDigits[0]*100)) < 20) {return `${dic.digits[arrDigits[0]]} hundred` + toReadable20(number)}
+
+  }
+  
+  return 'r'
 }
