8d1946d41d92a2a266b5cea09cf5cf9da384b760
bizarre84 <bizarre.san@gmail.com>
2020-10-25 23:32:05 +0300

    feat: impliment 100-120

--- src/index.js
+++ src/index.js
@@ -12,15 +12,18 @@ module.exports = function toReadable (number) {
     return (number%10 != 0) ? `${dic.decades[arrDigits[0]-2]} ${dic.digits[arrDigits[1]]}` : `${dic.decades[arrDigits[0]-2]}`
   }
   
-  
-  
 
   if (number < 20) return toReadable20(number)
   if (number >=20 && number < 100) return toReadable99(number)
   if (number >=100 && number < 1000) {
     if (number%100 == 0) {return `${dic.digits[arrDigits[0]]} hundred`}
-    if ((number - (arrDigits[0]*100)) < 20) {return `${dic.digits[arrDigits[0]]} hundred` + toReadable20(number)}
-
+    if ((number - (arrDigits[0]*100)) < 20) {
+        return `${dic.digits[arrDigits[0]]} hundred ${toReadable20((number - (arrDigits[0]*100)))}`
+    }
+    if ((number - (arrDigits[0]*100)) >= 20) {
+        arrDigits = (number - (arrDigits[0]*100)).toString().split('').map(Number)
+        return `${dic.digits[arrDigits[0]]} hundred ${toReadable99((number - (arrDigits[0]*100)))}`
+    }
   }
   
   return 'r'
